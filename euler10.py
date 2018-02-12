effc65736e40a482ff2fb500b15c3511914fb1b7
Den Dekker Benjamin <bdendekk@SA203-07A.ensi-bourges.fr>
2018-02-12 14:43:50 +0100

    Problem 10

---
+++ euler10.py
@@ -0,0 +1,17 @@
+from math import sqrt,floor
+
+def is_prime(n):
+    i=1
+    for i in range(2,floor(sqrt(n))+1 ):
+        if n%i == 0:
+            return False
+    return True
+
+
+
+i=1
+s=0
+for i in range(1,2000000):
+    if is_prime(i):
+        s+=i
+print(s-1)
